fb07e999b0decb0547b5cee12cce4ef858919761
eddineimad0 <zerrouq.imad@outlook.com>
2023-03-03 19:16:12 +0100

    No longer requiring Neodev.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,9 +53,6 @@ local on_attach = function(_, bufnr)
     rust_analyzer = {},
   }
   
-  -- Setup neovim lua configuration
-  require('neodev').setup()
-  --
   -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
