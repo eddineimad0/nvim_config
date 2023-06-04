ae96fb428972874756bbe2479617a0474e850787
eddineimad0 <zerrouq.imad@outlook.com>
2023-06-04 18:56:48 +0100

    Removed help from treesitters.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'rust', 'zig', 'lua', 'help'},
+  ensure_installed = { 'c', 'rust', 'zig', 'lua'},
 
   highlight = { enable = true },
   indent = { enable = true },
