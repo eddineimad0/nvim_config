5564522b3af16727219559745c1c00a366ba85a7
eddineimad0 <zerrouq.imad@outlook.com>
2023-08-05 17:44:36 +0100

    added live_grep option.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -13,14 +13,16 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- Keymaps  
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind existing buffers' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]earch [D]iagnostics' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind existing buffers' })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+
 vim.keymap.set('n', '<leader>/', function()
-require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
   winblend = 10,
   previewer = false,
 })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+end, { desc = '[/] Fuzzily search in current buffer' })
 
