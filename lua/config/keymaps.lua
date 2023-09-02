1ee26df550b00c5b14964d6f82bcc33f3a353bcf
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2023-09-02 12:10:44 +0100

    removed unused keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -78,15 +78,3 @@ map("i","<C-e>","<End>",{ desc = "Goto line end", noremap = true })
 
 -- Keymaps for better default experience
 map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true, noremap = true })
-
--- toggle options
--- map("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Toggle format on Save" })
--- map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
--- map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
--- map("n", "<leader>ul", function() Util.toggle_number() end, { desc = "Toggle Line Numbers" })
--- map("n", "<leader>ud", Util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
--- local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
--- map("n", "<leader>uc", function() Util.toggle("conceallevel", false, {0, conceallevel}) end, { desc = "Toggle Conceal" })
--- if vim.lsp.inlay_hint then
---   map("n", "<leader>uh", function() vim.lsp.inlay_hint(0, nil) end, { desc = "Toggle Inlay Hints" })
--- end
